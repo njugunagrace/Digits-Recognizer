e7db6f8760ff06a923e9d3d5281100fab74f84f0
njugunagrace <njugunagraceewambui@gmail.com>
2024-11-08 22:27:06 +0300

    Commented dashboard

--- digit_recognition/settings.py
+++ digit_recognition/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wtbku6ijjz!=janc!!#!p9rc&+oo4g+&5ck=h0)(%g$28-w=!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
